565a558fa3fde81d77b6149e81fe45497c678af5
JulioCortazar2307 <jccortazar0@soy.sena.edu.co>
2025-07-07 17:08:25 -0500

    ejercicio completado

--- model/aprendiz/index.php
+++ model/aprendiz/index.php
@@ -8,21 +8,19 @@ $con = $db->conectar();
 
 <?php
 if (isset($_GET['cerrar_sesion'])) {
-    session_destroy();
     header("location:../../index.php");
+    session_destroy();
     exit;
 } 
 ?>
 
 <html>
     <body>
-        
-    
-<h1>Hola <?php echo$_SESSION['nombre']; ?></h1>
-<br>
-<h2>Para salir presione el boton</h2>
-<form action="get">
-    <button type="submit" name="cerrar_sesion">Cerrar Sesión</button>
-</form>
-</body>
+        <h1>Hola <?php echo$_SESSION['nombre']; ?></h1>
+        <br>
+        <h2>Para salir presione el boton</h2>
+        <form action="get">
+            <button type="submit" name="cerrar_sesion">Cerrar Sesión</button>
+        </form>
+    </body>
 </html>
